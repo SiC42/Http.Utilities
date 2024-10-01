5ad27bec71028aaebc4e0feb54ab48d777610ed3
Simon Bordewisch <sbordewisch@gmx.de>
2024-10-01 12:31:50 +0200

    Add test for Immutable Builder

--- test/UrlBuilding.Tests/ImmutableUrlBuilderTests.cs
+++ test/UrlBuilding.Tests/ImmutableUrlBuilderTests.cs
@@ -8,4 +8,30 @@ internal class ImmutableUrlBuilderTest : UrlBuilderTestBase<ImmutableUrlBuilder>
     {
         return new ImmutableUrlBuilder(urlStr);
     }
+
+
+    [Test]
+    public void WhenAllMethodsCalled_OriginalBuilderNotChanged()
+    {
+        var initalUrl = new Uri("http://test.de/{Hello}?base=setup");
+        var builder = new ImmutableUrlBuilder(initalUrl)
+            .WithPathValue("Hello", "hello");
+
+        var finalUrl = builder
+            .SetScheme("https")
+            .SetHost("new-host.com")
+            .SetPort(4242)
+            .WithPathValue("Hello", "hi")
+            .AddPath("world")
+            .AddPath("word")
+            .AddQuery("this", "is")
+            .AddQuery("a", "test")
+            .Build();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(builder.Build().ToString(), Is.EqualTo("http://test.de/hello?base=setup"));
+            Assert.That(finalUrl.ToString(), Is.EqualTo("https://new-host.com:4242/hi/world/word?base=setup&this=is&a=test"));
+        });
+    }
 }
